ce7f1dde4966bfe90bd97ee68015918a794a88b0
Michal Jenis <jenis.michal@gmail.com>
2025-04-12 13:53:19 +0200

    Enable Xdebug on demand

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-echo "xdebug.mode=${XDEBUG_MODE}" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-echo "xdebug.start_with_request=${XDEBUG_START_WITH_REQUEST}" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-echo "${XDEBUG_CONFIG}" | tr " " "\n" | sed 's/^/xdebug./' >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+if [ "$XDEBUG_ENABLE" = "true" ]; then
+    echo "Enabling Xdebug"
+    docker-php-ext-enable xdebug
+    echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    echo "xdebug.mode=${XDEBUG_MODE}" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    echo "xdebug.start_with_request=${XDEBUG_START_WITH_REQUEST}" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    echo "${XDEBUG_CONFIG}" | tr " " "\n" | sed 's/^/xdebug./' >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+else
+    echo "Xdebug is disabled"
+    rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
 
 # start the main service
 php-fpm
